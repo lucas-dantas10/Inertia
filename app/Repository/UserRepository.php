46fbcb285de1498d0ceffcc5ef5158a43c6309bf
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-09-29 10:45:14 -0300

    abstractRepository implementando a repositoryInterface e userRepository extendendo de abstract e podendo fazer a expansão de métodos

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Repository;
+
+use App\Models\User;
+
+class UserRepository extends AbstractRepository
+{
+    protected static $model = User::class;
+
+    public static function findByEmail(string $email)
+    {
+        return self::loadModel()::query()->where(['email' => $email])->first();
+    }
+}
